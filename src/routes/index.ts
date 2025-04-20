140ea59dcb72326a0250f5d299cf08fba085519b
Gabriel <bielrj170@gmail.com>
2025-04-19 23:11:17 -0300

    feat: import new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,16 +2,28 @@ import express, { type Request, type Response, type Express } from 'express';
 import cors from 'cors';
 import authRoutes from '../modules/auth/routes/auth-routes';
 import groupRoutes from '../modules/group/routes/group-routes';
+import userRoutes from '../modules/users/routes/user-routes';
+import checkinRoutes from '../modules/checkin/routes/checkin-routes';
+import messageRoutes from '../modules/message/routes/message-routes';
+import authMiddleware from '../modules/auth/middleware/auth-middleware';
 
 const setupRoutes = (app: Express) => {
   app.use(express.json());
   app.use(cors());
-  app.get('/server-test', (req: Request, res: Response) => {
+
+  const apiRouter = express.Router();
+
+  apiRouter.get('/server-test', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Server is running!' });
   });
 
-  app.use(authRoutes);
-  app.use(groupRoutes);
+  apiRouter.use(authRoutes);
+  apiRouter.use(authMiddleware)
+  apiRouter.use(groupRoutes);
+  apiRouter.use(userRoutes);
+  apiRouter.use(checkinRoutes);
+  apiRouter.use(messageRoutes);
+  app.use('/api/v1', apiRouter);
 };
 
 export { setupRoutes };
