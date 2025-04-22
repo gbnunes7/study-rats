cccc8b099f6209ec1bc6cd524a512af6d86509b5
Gabriel <bielrj170@gmail.com>
2025-04-21 21:40:17 -0300

    feat: create a register route

---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import FormRegister from "@/components/FormRegister";
+
+
+const Register = () => {
+
+	return (
+		<div className="flex-grow flex flex-col items-center justify-center px-8 py-8 bg-black">
+			<FormRegister />
+		</div>
+	);
+};
+
+export default Register;
