7ff05178a346426dd91d8b4e1e8ccaf5d1bda07e
Gabriel <bielrj170@gmail.com>
2025-04-28 23:53:52 -0300

    feat: insert the menu component in main container

--- frontend/src/components/ContainerMain/index.tsx
+++ frontend/src/components/ContainerMain/index.tsx
@@ -1,3 +1,5 @@
+import MenuHamburger from "../MenuHamburger";
+
 interface ChildrenProps {
 	children: React.ReactNode;
 }
@@ -5,6 +7,9 @@ interface ChildrenProps {
 const ContainerMain: React.FC<ChildrenProps> = ({ children }) => {
 	return (
 		<div className="flex-grow flex flex-col items-center px-4 py-8 bg-black">
+			<div className="flex flex-col items-start mb-4 w-full">
+				<MenuHamburger />
+			</div>
 			{children}
 		</div>
 	);
